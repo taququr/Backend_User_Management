55cb4973aa7b366a9c9cbf0a18d0600cc9fa5e3c
Taqie Fadlillah <taqief@hexacode.org>
2023-03-16 15:28:53 +0700

    feat: add roles

--- controllers/UsersControllers.js
+++ controllers/UsersControllers.js
@@ -2,17 +2,7 @@ const { v4: uuid } = require("uuid");
 const { validationResult } = require("express-validator");
 
 const HttpError = require("../models/HttpError");
-// const { connection } = require("../models/MysqlConnection");
 const mysql = require("mysql2/promise");
-
-let DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "Taqie Fadlillah",
-    email: "test@test.com",
-    password: "testtest",
-  },
-];
 
 const getUsers = async (req, res, next) => {
   const connection = await mysql.createConnection({
@@ -26,7 +16,9 @@ const getUsers = async (req, res, next) => {
     return next(new HttpError(err), 422);
   }
 
-  const [rows, fields] = await connection.query("select * from users");
+  const [rows, fields] = await connection.query(
+    "select users.name as name, users.email, roles.name as roles from users join roles on users.rolesId = roles.id"
+  );
 
   if (!rows) {
     res.status(404);
@@ -35,123 +27,6 @@ const getUsers = async (req, res, next) => {
 
   res.status(200);
   res.send({ data: rows });
-  connection.destroy();
-};
-
-const createUser = async (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return next(new HttpError("Invalid inputs, please check your data"), 422);
-  }
-
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "0000",
-    database: "backend_user_management",
-  });
-
-  if (!connection) {
-    return next(new HttpError(err), 422);
-  }
-
-  const { name, email, password } = req.body;
-  const [emailFound] = await connection.execute(
-    "select * from users where email = ?",
-    [email]
-  );
-
-  if (emailFound.length > 0) {
-    return next(
-      new HttpError("Could not create user, email already exists"),
-      422
-    );
-  }
-
-  const [rows] = await connection.execute(
-    "INSERT INTO users(name, email, password) values(?, ?, ?)",
-    [name, email, password]
-  );
-
-  if (rows.length === 0) {
-    return next(new HttpError(err, 422));
-  }
-
-  res.status(201);
-  res.send({ message: "Successfully created", data: rows });
-  connection.destroy();
-};
-
-const updateUser = async (req, res, next) => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "0000",
-    database: "backend_user_management",
-  });
-
-  const { email, password, newName, newEmail, newPassword } = req.body;
-
-  if (!newName && !newEmail && !newPassword) {
-    return next(new HttpError("Please insert valid data"), 500);
-  }
-
-  const [emailFound] = await connection.execute(
-    "select * from users where email = ?",
-    [email]
-  );
-
-  if (emailFound.length <= 0) {
-    return next(new HttpError("User not found"), 422);
-  } else if (emailFound[0].password !== password) {
-    return next(new HttpError("Credential seems to be wrong"), 500);
-  }
-
-  const [updatedUser] = await connection.execute(
-    "update users set name = ?, email = ?, password = ? where email = ?",
-    [
-      newName ? newName : emailFound[0].name,
-      newEmail ? newEmail : emailFound[0].email,
-      newPassword ? newPassword : emailFound[0].password,
-      email,
-    ]
-  );
-
-  if (updatedUser.length <= 0) {
-    return next(new HttpError("There's seems to be something wrong"), 500);
-  }
-
-  res.json({ message: "Successfully updated", data: updatedUser });
-  connection.destroy();
-};
-
-const deleteUser = async (req, res, next) => {
-  const connection = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "0000",
-    database: "backend_user_management",
-  });
-
-  const { email, password } = req.body;
-
-  const [emailFound] = await connection.execute(
-    "select * from users where email = ?",
-    [email]
-  );
-
-  if (emailFound.length <= 0) {
-    return next(new HttpError("User not found"), 422);
-  } else if (emailFound[0].password !== password) {
-    return next(new HttpError("Credential seems to be wrong"), 500);
-  }
-
-  const [userDeleted] = await connection.execute(
-    "delete from users where email = ?",
-    [email]
-  );
-
-  res.json({ message: "User deleted", data: userDeleted });
   connection.destroy();
 };
 
@@ -178,8 +53,145 @@ const login = async (req, res, next) => {
   connection.destroy();
 };
 
+const createUser = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return next(new HttpError("Invalid inputs, please check your data"), 422);
+  }
+
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "0000",
+    database: "backend_user_management",
+  });
+
+  if (!connection) {
+    return next(new HttpError(err), 422);
+  }
+
+  const { name, email, password, rolesId, creatorRoles } = req.body;
+
+  if (creatorRoles !== "admin") {
+    return next(new HttpError("Account not authorize"), 401);
+  }
+
+  const [emailFound] = await connection.execute(
+    "select users.name as name, roles.name as roles from users join roles on users.rolesId = roles.id where users.email = ?",
+    [email]
+  );
+
+  if (emailFound.length > 0) {
+    return next(
+      new HttpError("Could not create user, email already exists"),
+      422
+    );
+  }
+
+  const [rows] = await connection.execute(
+    "INSERT INTO users(name, email, password, rolesId) values(?, ?, ?, ?)",
+    [name, email, password, rolesId]
+  );
+
+  if (rows.length === 0) {
+    return next(new HttpError(err, 422));
+  }
+
+  res.status(201);
+  res.send({ message: "Successfully created", data: rows });
+  connection.destroy();
+};
+
+const updateUser = async (req, res, next) => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "0000",
+    database: "backend_user_management",
+  });
+
+  const {
+    email,
+    password,
+    updaterRoles,
+    newName,
+    newEmail,
+    newPassword,
+    newRolesId,
+  } = req.body;
+
+  if (updaterRoles !== "admin") {
+    return next(new HttpError("Account not authorize"), 500);
+  } else if (!newName && !newEmail && !newPassword && !newRolesId) {
+    return next(new HttpError("Please insert valid data"), 500);
+  }
+
+  const [emailFound] = await connection.execute(
+    "select * from users where email = ?",
+    [email]
+  );
+
+  if (emailFound.length <= 0) {
+    return next(new HttpError("User not found"), 422);
+  } else if (emailFound[0].password !== password) {
+    return next(new HttpError("Credential seems to be wrong"), 500);
+  }
+
+  const [updatedUser] = await connection.execute(
+    "update users set name = ?, email = ?, password = ?, rolesId = ? where email = ?",
+    [
+      newName ? newName : emailFound[0].name,
+      newEmail ? newEmail : emailFound[0].email,
+      newPassword ? newPassword : emailFound[0].password,
+      newRolesId ? newRolesId : emailFound[0].rolesId,
+      email,
+    ]
+  );
+
+  if (updatedUser.length <= 0) {
+    return next(new HttpError("There's seems to be something wrong"), 500);
+  }
+
+  res.json({ message: "Successfully updated", data: updatedUser });
+  connection.destroy();
+};
+
+const deleteUser = async (req, res, next) => {
+  const connection = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "0000",
+    database: "backend_user_management",
+  });
+
+  const { email, password, deleterRoles } = req.body;
+
+  if (deleterRoles !== "admin") {
+    return next(new HttpError("Account not authorize"), 401);
+  }
+
+  const [emailFound] = await connection.execute(
+    "select * from users where email = ?",
+    [email]
+  );
+
+  if (emailFound.length <= 0) {
+    return next(new HttpError("User not found"), 422);
+  } else if (emailFound[0].password !== password) {
+    return next(new HttpError("Credential seems to be wrong"), 500);
+  }
+
+  const [userDeleted] = await connection.execute(
+    "delete from users where email = ?",
+    [email]
+  );
+
+  res.json({ message: "User deleted", data: userDeleted });
+  connection.destroy();
+};
+
 exports.getUsers = getUsers;
+exports.login = login;
 exports.createUser = createUser;
 exports.updateUser = updateUser;
 exports.deleteUser = deleteUser;
-exports.login = login;
--- routes/UsersRoutes.js
+++ routes/UsersRoutes.js
@@ -7,25 +7,18 @@ const router = express.Router();
 
 router.get("/", UsersControllers.getUsers);
 
+router.post("/login", UsersControllers.login);
+
 router.post(
   "/create",
   [
     check("name").notEmpty(),
     check("email").normalizeEmail().isEmail(),
     check("password").isLength({ min: 6 }),
+    check("rolesId").notEmpty(),
+    check("creatorRoles").notEmpty(),
   ],
   UsersControllers.createUser
-);
-
-router.post("/login", UsersControllers.login);
-
-router.delete(
-  "/delete",
-  [
-    check("email").normalizeEmail().isEmail(),
-    check("password").isLength({ min: 6 }),
-  ],
-  UsersControllers.deleteUser
 );
 
 router.put(
@@ -33,8 +26,19 @@ router.put(
   [
     check("email").normalizeEmail().isEmail(),
     check("password").isLength({ min: 6 }),
+    check("updaterRoles").notEmpty(),
   ],
   UsersControllers.updateUser
+);
+
+router.delete(
+  "/delete",
+  [
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+    check("deleterRoles").notEmpty(),
+  ],
+  UsersControllers.deleteUser
 );
 
 module.exports = router;
